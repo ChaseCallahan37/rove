ea74d3d5799241f67344a967c48daf391a97e101
Chase Callahan <ctcallahan2@crimson.ua.edu>
2024-01-30 06:58:55 -0600

    created basic marker rendering

--- mobile-client/src/components/EventMap.tsx
+++ mobile-client/src/components/EventMap.tsx
@@ -1,9 +1,26 @@
 import { PropsWithChildren } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import MapView, { PROVIDER_DEFAULT, PROVIDER_GOOGLE } from "react-native-maps";
+import MapView, { Marker, PROVIDER_DEFAULT, PROVIDER_GOOGLE } from "react-native-maps";
 
 type SectionProps = PropsWithChildren<{
 }>;
+
+
+const events  = [{
+  coordinate: {latitude: 33.2098,
+  longitude: -87.5692} 
+},
+{
+  coordinate: {latitude: 33.2325,
+  longitude: -87.5211} 
+},{
+  coordinate: {latitude: 33.2341,
+  longitude: -87.5331} 
+},{
+  coordinate: {latitude: 33.2433,
+  longitude: -87.5909} 
+},
+]
 
 function EventMap(): React.JSX.Element {
     return (
@@ -18,6 +35,17 @@ function EventMap(): React.JSX.Element {
          longitudeDelta: 0.0121,
        }}
      >
+      {events.map(({coordinate}, index) => 
+        <Marker 
+        key={index}
+          coordinate={coordinate}
+        />)}
+      <Marker 
+        coordinate={{
+         latitude: 33.2098,
+         longitude: -87.5692 
+        }}
+      />
      </MapView>
    </View>
     );
