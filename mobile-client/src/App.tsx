dff051b8c250138f0d01d80d287ebab5e64eb45d
Chase Callahan <ctcallahan2@crimson.ua.edu>
2024-02-05 21:38:57 -0600

    added EventMapScreen component to be used for rendering when navigating to event map screen

--- mobile-client/src/App.tsx
+++ mobile-client/src/App.tsx
@@ -1,47 +1,16 @@
 import React from "react";
-import type { PropsWithChildren } from "react";
-import {
-  Button,
-  Text,
-  View,
-} from "react-native";
-
 import { enableLatestRenderer } from "react-native-maps";
+
 import AppNavigation from "./navigation/AppNavigations";
 
 // We do this to allow the react-native-maps library to properly render
 // the most recent and up to date version of maps.
 enableLatestRenderer();
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
 function App(): React.JSX.Element {
   return (
    <AppNavigation /> 
   );
-}
-
-// @ts-ignore
-const HomeScreen = ({navigation}) => {
-  return ( 
-  <Button 
-    title="Go to Jane's profile"
-    onPress={() => navigation.navigate("Profile", {name: "Jane"})}
-  />
-  )
-}
-
-// @ts-ignore
-const ProfileScreen = ({navigation, route}) => {
-  return (
-    <View>
-
-    <Text style={{color: "blue"}}>This is {route.params.name}'s profile</Text>
-    <Button title={route.params.name}/>
-    </View>
-  )
 }
 
 export default App;
--- mobile-client/src/navigation/AppNavigations.tsx
+++ mobile-client/src/navigation/AppNavigations.tsx
@@ -1,6 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import HomeScreen from "./screens/HomeScreen"
+import EventMap from "../components/EventMap"
 
 // We want to define the routes here and export them throughout our app
 // so that we do not mispell the name of the routes in other places
@@ -19,57 +20,13 @@ function AppNavigation() {
                     name={routes.home}
                     component={HomeScreen}
                 />
+                <Stack.Screen 
+                    name={routes.eventMap}
+                    component={EventMap}
+                />
             </Stack.Navigator>
         </NavigationContainer>
     )
 }
 
 export default AppNavigation
-
-
-
-
-// function App(): React.JSX.Element {
-//     return (
-//       <NavigationContainer>
-//         <Stack.Navigator>
-//           <Stack.Screen
-//             name="Home"
-//             component={HomeScreen}
-//             options={{
-//               title: "Welcome"
-//             }}
-//             />
-//             <Stack.Screen 
-//               name="Profile"
-//               component={ProfileScreen}
-//             />
-//           {/* <View style={{ flex: 1 }}>
-//             <EventMap />
-//           </View> */}
-//         </Stack.Navigator>
-//       </NavigationContainer>
-//     );
-//   }
-  
-//   // @ts-ignore
-//   const HomeScreen = ({navigation}) => {
-//     return ( 
-//     <Button 
-//       title="Go to Jane's profile"
-//       onPress={() => navigation.navigate("Profile", {name: "Jane"})}
-//     />
-//     )
-//   }
-  
-//   // @ts-ignore
-//   const ProfileScreen = ({navigation, route}) => {
-//     return (
-//       <View>
-  
-//       <Text style={{color: "blue"}}>This is {route.params.name}'s profile</Text>
-//       <Button title={route.params.name}/>
-//       </View>
-//     )
-//   }
-  
