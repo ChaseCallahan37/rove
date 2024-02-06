d08562ac315fd64d9dade2b00f756f800f8539af
Chase Callahan <ctcallahan2@crimson.ua.edu>
2024-02-05 20:41:06 -0600

    created script that will start database and server with one command

---
+++ web_api/bash/run_dev.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Run start_db.sh script
+./bash/start_db.sh
+
+# Check if start_db.sh succeeded
+if [ $? -eq 0 ]; then
+    echo "Database started successfully. Starting Phoenix server..."
+    # Run the Phoenix server
+    mix phx.server
+else
+    echo "Failed to start database. Aborting..."
+    exit 1
+fi
