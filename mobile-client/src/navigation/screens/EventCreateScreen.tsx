c24a9587e34f2f1f0b78cc60bc4a18e677e46cb5
Chase Callahan <ctcallahan2@crimson.ua.edu>
2024-02-07 13:22:27 -0600

    added hook to make use of toggles abstracted

--- mobile-client/src/navigation/screens/EventCreateScreen.tsx
+++ mobile-client/src/navigation/screens/EventCreateScreen.tsx
@@ -1,15 +1,31 @@
 import { useState } from "react";
 import { Text, TextInput, SafeAreaView, Button } from "react-native";
+import DatePicker from "react-native-date-picker"
+import useApi from "../../hooks/useApi";
+import { Event, createEvent } from "../../api/events/event";
 
 function EventCreateScreen() {
     const [eventType, setEventType] = useState("")
-    const [eventDate, setEventDate] = useState("")
+    const [eventDate, setEventDate] = useState(new Date())
     const [eventLatitude, setEventLatitude] = useState(0)
     const [eventLongitude, setEventLongitude] = useState(0)
     const [eventDescription, setEventDescription] = useState("")
 
+    const [open, setOpen] = useState(false)
+    const {request} = useApi(createEvent)
+    
     const handleSubmit = () => {
-        console.log("HANDLING SUBMIT");
+      const myEvent: Event = {
+       date: eventDate,
+        latitude: eventLatitude,
+        longitude: eventLongitude,
+        title: eventDescription 
+      }
+      
+      console.log(myEvent);
+        
+      
+      request(myEvent)   
         
     }
 
@@ -23,13 +39,19 @@ function EventCreateScreen() {
         keyboardType="ascii-capable"
         onChange={({nativeEvent: {text}}) => setEventType(text)}
         />
-        <TextInput
-        value={eventDate}
-        style={{backgroundColor: "black"}}
-        placeholder="Please enter date"
-        keyboardType="ascii-capable"
-        onChange={({nativeEvent: {text}}) => setEventDate(text)}
-        />
+        <Button title="Open" onPress={() => setOpen(true)} />
+      <DatePicker
+        modal
+        open={open}
+        date={eventDate}
+        onConfirm={(date) => {
+          setOpen(false)
+          setEventDate(date)
+        }}
+        onCancel={() => {
+          setOpen(false)
+        }}
+      />
         <TextInput
         value={eventLatitude.toLocaleString()}
         style={{backgroundColor: "black"}}
@@ -43,13 +65,6 @@ function EventCreateScreen() {
         placeholder="Please enter longitude"
         keyboardType="decimal-pad"
         onChange={({nativeEvent: {text}}) => setEventLongitude(parseInt(text))}
-        />
-        <TextInput
-        value={eventDate}
-        style={{backgroundColor: "black"}}
-        placeholder="Please enter description"
-        keyboardType="ascii-capable"
-        onChange={({nativeEvent: {text}}) => setEventDescription(text)}
         />
         <Button 
             title="Submit"
