74a6d31442884384ee82f1e31c903bd969d448ba
Chase Callahan <ctcallahan2@crimson.ua.edu>
2024-02-08 16:04:55 -0600

    Added moved handle on confirm to function and utilized the actual updated date

--- mobile-client/src/components/AppDatePicker.tsx
+++ mobile-client/src/components/AppDatePicker.tsx
@@ -2,6 +2,7 @@ import { Button, Text, View } from "react-native";
 import useToggle from "../hooks/useToggle";
 import DatePicker from "react-native-date-picker";
 import format from "../utils/format";
+import { setDate } from "date-fns";
 
 type AppDatePickerProps = {
   date: Date;
@@ -9,21 +10,24 @@ type AppDatePickerProps = {
 };
 
 function AppDatePicker({ date, updateDate }: AppDatePickerProps) {
-  const { isToggled: showDatePicker, toggle: toggleDatePicker } = useToggle(false);
+  const { isToggled: showDatePicker, toggle: toggleDatePicker } =
+    useToggle(false);
+
+    const handleOnConfirm = (date: Date) => {
+      updateDate(date)
+      toggleDatePicker()
+    } 
   return (
     <>
       <Button title={format.dateTime(date)} onPress={toggleDatePicker} />
       <DatePicker
         modal
         open={showDatePicker}
-        date={new Date()}
-        onConfirm={() => {
-          toggleDatePicker();
-          updateDate(date);
-        }}
+        date={date}
+        onConfirm={handleOnConfirm}
         onCancel={toggleDatePicker}
       />
-</>
+    </>
   );
 }
 
