98c8db6bd2efc21facc432afed9da3f9c128e5ad
Chase Callahan <ctcallahan2@crimson.ua.edu>
2024-02-08 11:53:07 -0600

    added util for short dates

---
+++ mobile-client/src/utils/format.ts
@@ -0,0 +1,12 @@
+function shortDate(date: Date) {
+  return date.toLocaleDateString("en-us", {
+    weekday: "long",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
+}
+
+export default {
+    shortDate
+}
