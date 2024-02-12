3ede2dd28343880e5f1d1d3fffec74261b0da56a
Chase Callahan <ctcallahan2@crimson.ua.edu>
2024-02-12 03:25:54 -0600

    Created component responsible for rendering list of events

--- mobile-client/src/navigation/screens/EventMapScreen.tsx
+++ mobile-client/src/navigation/screens/EventMapScreen.tsx
@@ -11,6 +11,7 @@ import { useEffect, useRef } from "react";
 import eventApi from "../../api/events";
 import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 import AppMapView from "../../components/AppMapView";
+import EventList from "../../components/EventList";
 
 function EventMapScreen() {
   const {
@@ -44,107 +45,13 @@ function EventMapScreen() {
         <Text>NEARBY</Text>
 
         {/*@ts-ignore          */}
-        <FlatList
-          data={events}
-          numColumns={2} // Adjust the number of columns as needed
-          // @ts-ignore
-          renderItem={({ item: { latitude, longitude, title } }) => (
-            <TouchableOpacity
-              onPress={() => {
-                if (!mapRef) return null;
-                // @ts-ignore
-                mapRef.current.animateToRegion(
-                  {
-                    latitude: latitude,
-                    longitude: longitude,
-                    latitudeDelta: 0.005,
-                    longitudeDelta: 0.005,
-                  },
-                  1000
-                );
-              }}
-              style={styles.card}
-            >
-              <TouchableOpacity style={styles.joinButton}>
-                <Text style={styles.joinButtonText}>Join</Text>
-              </TouchableOpacity>
-              <Image
-                style={styles.imagePlaceholder}
-                source={{
-                  uri: "https://cdn.dribbble.com/users/1409624/screenshots/11850998/media/445dea8b45ff2bf796545364620bccd4.png?resize=400x300&vertical=center",
-                }}
-              />
-              <Text style={styles.groupName}>Group Name</Text>
-              <Text style={styles.members}>{title}</Text>
-            </TouchableOpacity>
-          )}
+        <EventList 
+          mapRef={mapRef}
+          events={events}
         />
       </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  joinButton: {
-    alignSelf: "flex-start",
-    paddingVertical: 4,
-    paddingHorizontal: 6,
-    borderRadius: 4,
-  },
-
-  joinButtonText: {
-    color: "white",
-  },
-  imagePlaceholder: {
-    backgroundColor: "#bdc3c7",
-    height: 150, // Adjust the height as needed
-    width: "100%",
-    alignItems: "center",
-    justifyContent: "center",
-    marginVertical: 16,
-  },
-  groupInfo: {
-    alignItems: "center",
-  },
-  groupName: {
-    fontSize: 18,
-    fontWeight: "bold",
-  },
-  members: {
-    fontSize: 16,
-  },
-  card: {
-    width: "45%",
-    backgroundColor: "#d6f5d6", // This is a placeholder color
-    borderRadius: 10,
-    alignItems: "center",
-    justifyContent: "space-between",
-    elevation: 3, // Adds a subtle shadow on Android
-    shadowOpacity: 0.1, // Adds a subtle shadow on iOS
-    shadowRadius: 4,
-    shadowColor: "#000",
-    shadowOffset: { height: 2, width: 0 },
-    marginBottom: 10,
-    margin: "2.5%",
-  },
-  text: {
-    color: "white",
-    // Add more styling for your text as needed
-  },
-  container: {
-    // Fill the entire screen
-    flex: 1,
-  },
-  map: {
-    // Specify the height of the map or use flex to allocate space
-    height: 450, // You can adjust this value as needed
-    width: "100%",
-  },
-  eventList: {
-    // Flex to fill the remaining space after the map
-    flex: 1,
-  },
-  // Add more styles if needed
-});
 
 export default EventMapScreen;
