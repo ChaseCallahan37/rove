d043851f75accf3d38779bf514f2e16d17317985
Chase Callahan <ctcallahan2@crimson.ua.edu>
2024-02-16 08:30:40 -0600

    Created bubble label for event details screen

--- mobile-client/src/navigation/screens/EventDetailsScreen.tsx
+++ mobile-client/src/navigation/screens/EventDetailsScreen.tsx
@@ -4,6 +4,10 @@ import { Event, retrieveEvent } from "../../api/events/event";
 import useApi from "../../hooks/useApi";
 import { useEffect } from "react";
 
+
+import {style as tw} from "twrnc"
+import format from "../../utils/format";
+
 export type EventDetailsScreenParams = {
   eventID: string;
 };
@@ -20,17 +24,33 @@ function EventDetailsScreen({
   route: { params: {eventID}},
 }: EventDetailsScreenProps) {
 
-  const {loading, error, data: event, request: getEvent} = useApi(retrieveEvent)
-
+  const {loading, error, data, request: getEvent} = useApi(retrieveEvent)
+  const event = data as Event | undefined | null
   useEffect(() => {
     getEvent(eventID)
   }, [])
 
+console.log(event);
+
 
   return (
-    <View>
-      {loading ? <Text style={{color: "red"}}>Loading</Text> : error ? <Text style={{color: "red"}}>Error</Text> : 
-      <Text style={{ color: "blue" }}>This is new event!{JSON.stringify(event)}</Text>}
+    <View style={tw(['flex-1', 'bg-slate-100'])}>
+      {loading ? <Text style={{color: "red"}}>Loading</Text> : error ? <Text style={{color: "red"}}>Error</Text> :
+      event && 
+      <View style={tw(['grow', 'px-2'])}>
+        <Text style={{ color: "blue" }}>This is new event!{JSON.stringify(event)}</Text>
+        <View style={tw(['bg-sky-200', 'rounded-full', 'p-2', 'flex', 'items-center'])}>
+          <Text style={tw(['text-black', 'text-lg'])}>{event.title}</Text>
+          {// @ts-ignore
+          
+          <Text style={tw(['text-black'])}>{event.date}</Text>
+}
+        </View>
+      </View>
+      
+      
+      
+      }
     </View>
   );
 }
