6413a8ea483c48cb47a0a8c40aac26786c2a1601
Chase Callahan <ctcallahan2@crimson.ua.edu>
2024-02-20 05:25:53 -0600

    Extended functionality to allow for generation of JWT, validation of password, and authentication by providing email and password

--- web_api/lib/rove_api_web/auth/guardian.ex
+++ web_api/lib/rove_api_web/auth/guardian.ex
@@ -23,4 +23,23 @@ defmodule RoveApiWeb.Auth.Guardian do
     {:error, :no_id_provided}
   end
 
+  def authenticate(email, password) do
+    case Accounts.get_account_by_email(email) do
+      nil -> {:error, :unauthorized}
+      account ->
+        case validate_password(password, account.hash_password) do
+          true -> create_token(account)
+          false -> {:error, :unauthorized}
+        end
+    end
+  end
+
+  defp validate_password(password, hash_password) do
+    Bcrypt.verify_pass(password, hash_password)
+  end
+
+  defp create_token(account) do
+    {:ok, token, _claims} = encode_and_sign(account)
+    {:ok, account, token}
+  end
 end
