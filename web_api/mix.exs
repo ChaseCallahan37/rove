93e5ad7625d1bd576b72e46ae357a2eac5660fb2
Chase Callahan <ctcallahan2@crimson.ua.edu>
2024-02-23 08:16:40 -0600

    added guardian db as a dependency

--- web_api/mix.exs
+++ web_api/mix.exs
@@ -53,6 +53,7 @@ defmodule RoveApi.MixProject do
       {:plug_cowboy, "~> 2.5"},
       {:cors_plug, "~> 2.0"},
       {:guardian, "~> 2.3"},
+      {:guardian_db, "~> 2.0"},
       {:bcrypt_elixir, "~> 3.0"}
     ]
   end
