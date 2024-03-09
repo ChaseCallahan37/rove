9e5441bc735f5ceca170a56e8b13d3104a0add74
Chase Callahan <ctcallahan2@crimson.ua.edu>
2024-03-09 14:23:22 -0600

    Added function to update user profile

--- mobile-client/src/api/user/user.ts
+++ mobile-client/src/api/user/user.ts
@@ -1,8 +1,32 @@
+import createAuthHeader from "../../utils/createAuthHeader";
+import service from "../service";
+
 export type User = {
   id: string;
   user_name: string;
   first_name?: string;
   last_name?: string;
   dob?: Date;
-  gender?: "male" | "female" | "other";
+  gender?: string;
 };
+
+const resourceName = "users";
+
+export async function updateUserProfile(
+  token: string,
+  updatedFields: {
+    first_name: string;
+    last_name: string;
+    dob: Date;
+    gender: string;
+  }
+) {
+  await service.post(resourceName, {
+    headers: createAuthHeader(token),
+    payload: {
+      user: updatedFields,
+    },
+  });
+
+  return true;
+}
