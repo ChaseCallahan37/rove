b35748e73074cba5875455a2cc91302adc198165
Chase Callahan <ctcallahan2@crimson.ua.edu>
2024-03-12 12:05:39 -0500

    Added basic rendering list to event details views

--- mobile-client/src/navigation/screens/EventOwnerDetailsScreen.tsx
+++ mobile-client/src/navigation/screens/EventOwnerDetailsScreen.tsx
@@ -1,4 +1,4 @@
-import { Alert, Button, Image, Text, View } from "react-native";
+import { Alert, Button, Image, ScrollView, Text, View } from "react-native";
 import { style as tw } from "twrnc";
 
 import { AppNavigationProp } from "../AppNavigations";
@@ -7,6 +7,7 @@ import eventApi from "../../api/events";
 import { useCallback, useEffect } from "react";
 import useApi from "../../hooks/useApi";
 import { useFocusEffect } from "@react-navigation/native";
+import format from "../../utils/format";
 
 export type EventOwnerDetailsScreenParams = {
   eventId: string;
@@ -31,27 +32,12 @@ export default function EventOwnerDetailsScreen({
     data: event,
     request: getEvent,
   } = useApi(retrieveEvent);
-  const { request: joinEvent, data: joinEventOutcome } = useApi(
-    eventApi.joinEvent,
-    true
-  );
 
   useFocusEffect(
     useCallback(() => {
       getEvent(eventId);
-    }, [joinEventOutcome])
+    }, [])
   );
-
-  const handleOnJoin = async () => {
-    const success = await joinEvent(event?.id);
-
-    // @ts-ignore
-    if (success) {
-      Alert.alert("Successfully joined event!");
-    } else {
-      Alert.alert("Unable to join event, please try again");
-    }
-  };
 
   return (
     <View style={tw(["flex-1", "bg-slate-100", "py-8", "px-2"])}>
@@ -70,29 +56,8 @@ export default function EventOwnerDetailsScreen({
         <Text style={{ color: "red" }}>Error</Text>
       ) : (
         event && (
-          <View style={tw(["grow", "px-2"])}>
-            <View
-              style={tw([
-                "bg-sky-200",
-                "rounded-full",
-                "p-2",
-                "flex",
-                "flex-row",
-                "items-center",
-                "justify-around",
-              ])}
-            >
-              <Text style={tw(["text-black", "font-bold", "text-lg"])}>
-                {event.title}
-              </Text>
-              {
-                // @ts-ignore
-
-                <Text style={tw(["text-black", "text-sm"])}>{event.date}</Text>
-              }
-            </View>
-
-            {event.owner && (
+          <ScrollView>
+            <View style={tw(["grow", "px-2"])}>
               <View
                 style={tw([
                   "bg-sky-200",
@@ -105,27 +70,62 @@ export default function EventOwnerDetailsScreen({
                 ])}
               >
                 <Text style={tw(["text-black", "font-bold", "text-lg"])}>
-                  Created By: {event.owner.user_name}
+                  {event.title}
                 </Text>
-              </View>
-            )}
-            <View style={tw(["mt-16", "rounded", "bg-slate-200"])}>
-              <Image
-                style={tw(["w-full", "h-72", "rounded-t"])}
-                source={{
-                  uri: "https://cdn.dribbble.com/users/1409624/screenshots/11850998/media/445dea8b45ff2bf796545364620bccd4.png?resize=400x300&vertical=center",
-                }}
-              />
+                {
 
-              <View style={tw(["p-2"])}>
-                <Text style={tw(["text-black", "text-sm"])}>{event.title}</Text>
-                <Text style={tw(["text-black", "text-base", "font-semibold"])}>
-                  25 Members
-                </Text>
+                  <Text style={tw(["text-black", "text-sm"])}>
+                    {format.shortDate(event.date)}
+                  </Text>
+                }
+              </View>
+
+              {event.owner && (
+                <View
+                  style={tw([
+                    "bg-sky-200",
+                    "rounded-full",
+                    "p-2",
+                    "flex",
+                    "flex-row",
+                    "items-center",
+                    "justify-around",
+                  ])}
+                >
+                  <Text style={tw(["text-black", "font-bold", "text-lg"])}>
+                    Created By: {event.owner.user_name}
+                  </Text>
+                </View>
+              )}
+              <View style={tw(["mt-16", "rounded", "bg-slate-200"])}>
+                <Image
+                  style={tw(["w-full", "h-72", "rounded-t"])}
+                  source={{
+                    uri: "https://cdn.dribbble.com/users/1409624/screenshots/11850998/media/445dea8b45ff2bf796545364620bccd4.png?resize=400x300&vertical=center",
+                  }}
+                />
+
+                <View style={tw(["p-2"])}>
+                  <Text style={tw(["text-black", "text-sm"])}>
+                    {event.title}
+                  </Text>
+                  <Text
+                    style={tw(["text-black", "text-base", "font-semibold"])}
+                  >
+                  {`${event.attendees?.length} Attendee${
+                      event.attendees?.length !== 1 ? "s" : ""
+                    }`}
+                  </Text>
+                </View>
               </View>
             </View>
-            <Button title="Join Event" onPress={handleOnJoin} />
-          </View>
+            <Text style={tw(["text-black"])}>Attendees</Text>
+            {event.attendees?.map(({ user_name }, index) => (
+              <Text key={index} style={tw(["text-black"])}>
+                {user_name}
+              </Text>
+            ))}
+          </ScrollView>
         )
       )}
     </View>
