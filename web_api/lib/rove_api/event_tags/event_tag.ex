ca57b4b83284659de63065ed219493df6d0c75cc
Chase Callahan <ctcallahan2@crimson.ua.edu>
2024-03-14 16:53:59 -0500

    Added event_tag schema

---
+++ web_api/lib/rove_api/event_tags/event_tag.ex
@@ -0,0 +1,30 @@
+defmodule RoveApi.EventAttendances.EventAttendance do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "event_tags" do
+    belongs_to :tag, RoveApi.Tags.Tag,
+      foreign_key: :tag_id,
+      type: :binary_id
+
+    belongs_to :event, RoveApi.Events.Event,
+      foreign_key: :event_id,
+      references: :id,
+      type: :binary_id
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(event, attrs) do
+    event
+    |> cast(attrs, [:tag_id, :event_id])
+    |> validate_required([:tag_id, :event_id])
+    |> unique_constraint([:tag_id, :event_id],
+      name: :unique_event_tags,
+      message: "Event has already been associated with this tag"
+    )
+  end
+end
