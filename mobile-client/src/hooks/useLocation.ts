f63829cebb6a7f94e28cd8887dc862ea45e9377c
Chase Callahan <ctcallahan2@crimson.ua.edu>
2024-03-19 13:56:31 -0500

    Configured app to handle location library for mbile app

--- mobile-client/src/hooks/useLocation.ts
+++ mobile-client/src/hooks/useLocation.ts
@@ -1,6 +1,8 @@
 
 import { useEffect, useState } from 'react';
 
+import Geolocation from '@react-native-community/geolocation';
+
 
 async function requestLocationPermission() {
  
@@ -13,6 +15,9 @@ async function getLocation(){
 export default function useLocation(){
     const [location, setLocation] = useState<undefined|{longitude: number, latitude: number}>()
 
+
+//@ts-ignore
+Geolocation.getCurrentPosition((info) => console.log(info), (er) => console.log(er), {});
     useEffect(() => {
         getLocation()
     })
