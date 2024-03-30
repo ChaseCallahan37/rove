d4c832a7d6ed90ad09c04a70c431b0c9614943a5
Chase Callahan <ctcallahan2@crimson.ua.edu>
2024-03-30 14:21:59 -0500

    working on geospatial filtering logic for events

--- web_api/lib/rove_api/search/events.ex
+++ web_api/lib/rove_api/search/events.ex
@@ -33,6 +33,37 @@ defmodule RoveApi.Search.Events do
     build_conditions(popped_params, dynamic([e], e.id in subquery(tags_query) and ^conditions))
   end
 
+  defp build_conditions(%{"location" => %{"latitude" => latitude, "longitude" => longitude, "radius" => radius}} = params, conditions) do
+    {_location, popped_params} = Map.pop(params, "location")
+
+    latitude = String.to_float(latitude)
+    longitude = String.to_float(longitude)
+    radius = String.to_float(radius)
+
+    # build_conditions(popped_params, dynamic([e],
+    #   ^radius >= calculate_distance({e.latitude, e.longitude}, {^latitude, ^longitude})
+    #   and ^conditions))
+  end
+
+  def calculate_distance(coord1, coord2) do
+    {lat1, lon1} = coord1
+    {lat2, lon2} = coord2
+
+    delta_lat = deg2rad(lat2 - lat1)
+    delta_lon = deg2rad(lon2 - lon1)
+
+    a = :math.sin(delta_lat / 2) * :math.sin(delta_lat / 2) +
+        :math.cos(deg2rad(lat1)) * :math.cos(deg2rad(lat2)) *
+        :math.sin(delta_lon / 2) * :math.sin(delta_lon / 2)
+
+    c = 2 * :math.atan2(:math.sqrt(a), :math.sqrt(1 - a))
+
+    radius_of_earth_km = 6371
+    radius_of_earth_km * c
+  end
+
+  defp deg2rad(deg), do: deg * (:math.pi / 180)
+
   defp build_conditions(_params, conditions) do
     conditions
   end
