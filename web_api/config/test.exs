53d0b143f0bd4fad8a3dccd14fb31454b509fd1e
Chase Callahan <ctcallahan2@crimson.ua.edu>
2024-04-20 16:48:56 -0500

    hi

--- web_api/config/test.exs
+++ web_api/config/test.exs
@@ -6,16 +6,16 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :rove_api, RoveApi.Repo,
-  username: "rove_api",
-  password: "CaYglGHMbZRK7QTmIZy/STizxoFMUR34PY/U/QEqEQgJwdQfCGpBnXEE3d9oIcSe",
-  hostname: "localhost",
-  database: "rove_api_dev_db",
-  port: 5432,
+  username: System.get_env("POSTGRES_USER"),
+  password: System.get_env("POSTGRES_PASSWORD"),
+  hostname: System.get_env("POSTGRES_HOST"),
+  database: System.get_env("POSTGRES_DATABASE"),
+  port: System.get_env("POSTGRES_PORT"),
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
-  pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10,
-  types: RoveApi.Repo.Types
+  types: RoveApi.Repo.Types,
+  pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
