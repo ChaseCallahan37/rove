a76fe1c0f1aaae3a83d7a54a99233385e91154cf
Chase Callahan <ctcallahan2@crimson.ua.edu>
2024-04-20 17:07:37 -0500

    Bringing over changes

--- web_api/bash/run_test.sh
+++ web_api/bash/run_test.sh
@@ -1,32 +1,10 @@
 #!/bin/bash
 
-# Get the absolute path to the web_api directory that contains the docker-compose.yml
-# Assuming the script is located in web_api/bash and you are running it from there
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+source ./.env/db.env
+source ./.env/app.test.env
 
-echo "Starting Docker services..."
-# Start Docker Compose services
-docker-compose -f "$DIR/docker-compose.yml" up -d
+docker-compose -f docker-compose.test.yml up --remove-orphans -d
 
-# Check if Docker Compose starts up successfully
-if [ $? -eq 0 ]; then
-    echo "Docker services started successfully."
-else
-    echo "Failed to start Docker services."
-    exit 1
-fi
-
-echo "Running mix tests..."
-# Run mix tests
-cd "$DIR"
 mix test
 
-# Capture the exit code of mix test
-TEST_EXIT_CODE=$?
-
-# Stop Docker services after tests
-echo "Stopping Docker services..."
-docker-compose -f "$DIR/docker-compose.yml" down
-
-# Exit with the exit code of the mix test
-exit $TEST_EXIT_CODE
+docker-compose -f docker-compose.test.yml down
